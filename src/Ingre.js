80fbad8e11ac013ffed697cc031de1092d46ef7c
Mohamed <reyashmohamed786@gmail.com>
2023-02-25 18:05:57 +0530

    add changes

--- src/Ingre.js
+++ src/Ingre.js
@@ -1,16 +1,7 @@
 import React from "react";
-const Ingre=({post})=>{
+const Ingre=()=>{
     return(
         <>
-                <div className="inner-box">
-                    <div className="info">
-                        <img src={post.PostImage} alt=""></img>
-                        <h1>{post.title}</h1>
-                        <h3>{post.author}</h3>
-                        <h4>{post.ingredients}</h4>
-                        <h4>{post.description}</h4>
-                    </div>
-                </div>
         </>
     )
 }
